c9e16d46ebe555de0c51a0f35320e15f761a0c98
Robinson <kramer@professor.educacao.sp.gov.br>
2025-02-14 12:10:13 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Olá mundo\n'
+  const msg = 'Fala dque eu te escuto\n'
   res.end(msg);
 });
 
