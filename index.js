1c8a6288597f793b706c6a89ef62982b4ec05666
Robinson <kramer@professor.educacao.sp.gov.br>
2025-02-17 16:37:30 -0300

    atualizei a msg

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Fala dque eu te escuto\n'
+  const msg = 'Fala que eu te escuto\n'
   res.end(msg);
 });
 
